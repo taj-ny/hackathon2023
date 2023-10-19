1fbeb8e0fcf97308fefbc02ec129e6197d58c845
d2dyno <53011783+d2dyno1@users.noreply.github.com>
2023-10-19 11:34:49 +0200

    Added Question view models

--- dobra3.Sdk/ViewModels/GameHostViewModel.cs
+++ dobra3.Sdk/ViewModels/GameHostViewModel.cs
@@ -1,14 +1,51 @@
-﻿using dobra3.Sdk.AppModels;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using dobra3.Sdk.AppModels;
+using dobra3.Sdk.DataModels;
+using dobra3.Shared.Utils;
 
 namespace dobra3.Sdk.ViewModels
 {
-    public sealed class GameHostViewModel : BasePageViewModel
+    public sealed partial class GameHostViewModel : BasePageViewModel, IAsyncInitialize
     {
-        private GameStateModel _gameState;
+        private readonly QuestionSetDataModel _questions;
+
+        [ObservableProperty] private ObservableCollection<QuestionViewModel> _Questions;
+        [ObservableProperty] private QuestionViewModel _CurrentQuestion;
 
         public GameHostViewModel()
         {
             
+        }
+
+        public Task InitAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        [RelayCommand]
+        public async Task A_AnswerAsync()
+        {
+
+        }
+
+        [RelayCommand]
+        public async Task B_AnswerAsync()
+        {
+
+        }
+
+        [RelayCommand]
+        public async Task C_AnswerAsync()
+        {
+
+        }
+
+        [RelayCommand]
+        public async Task D_AnswerAsync()
+        {
+
         }
     }
 }
---
+++ dobra3.Sdk/ViewModels/QuestionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace dobra3.Sdk.ViewModels
+{
+    public sealed partial class QuestionViewModel : ObservableObject
+    {
+        [ObservableProperty] private string _Title;
+
+        [ObservableProperty] private string _Description;
+    }
+}
