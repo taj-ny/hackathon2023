c87f57b802460048e77f00ed8818785f6118623b
taj-ny <79316397+taj-ny@users.noreply.github.com>
2023-10-19 11:32:21 +0200

    Add question deserialization

--- dobra3.Sdk/ViewModels/GameHostViewModel.cs
+++ dobra3.Sdk/ViewModels/GameHostViewModel.cs
@@ -1,14 +1,22 @@
 ﻿using dobra3.Sdk.AppModels;
+using dobra3.Sdk.DataModels;
+using dobra3.Shared.Utils;
 
 namespace dobra3.Sdk.ViewModels
 {
-    public sealed class GameHostViewModel : BasePageViewModel
+    public sealed class GameHostViewModel : BasePageViewModel, IAsyncInitialize
     {
         private GameStateModel _gameState;
+        private QuestionSetDataModel _questions;
 
-        public GameHostViewModel()
+        public GameHostViewModel(QuestionSetDataModel questions)
         {
-            
+            _questions = questions;
+        }
+
+        public Task InitAsync(CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
         }
     }
 }
