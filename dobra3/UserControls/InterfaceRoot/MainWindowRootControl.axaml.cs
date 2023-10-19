6d8e966c3e59fb600db794ac984086ed5892f8f5
d2dyno <53011783+d2dyno1@users.noreply.github.com>
2023-10-19 09:55:47 +0200

    Views

---
+++ dobra3/UserControls/InterfaceRoot/MainWindowRootControl.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace dobra3.UserControls.WindowRoot
+{
+    public partial class MainWindowRootControl : UserControl
+    {
+        public MainWindowRootControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
