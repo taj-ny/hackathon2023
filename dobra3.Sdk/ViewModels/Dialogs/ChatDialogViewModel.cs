090a94f8a5640c3de768e0014bf981a9074bc684
taj-ny <79316397+taj-ny@users.noreply.github.com>
2023-10-19 17:40:50 +0200

    Improved chat experience

--- dobra3.Sdk/ViewModels/Dialogs/ChatDialogViewModel.cs
+++ dobra3.Sdk/ViewModels/Dialogs/ChatDialogViewModel.cs
@@ -2,7 +2,6 @@
 using dobra3.Shared.Utils;
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.Input;
-using dobra3.Sdk.DataModels;
 using dobra3.Shared.Api;
 using OpenAI_API.Chat;
 using OpenAI_API.Models;
@@ -12,6 +11,7 @@ namespace dobra3.Sdk.ViewModels.Dialogs
     public sealed partial class ChatDialogViewModel : ObservableObject, IAsyncInitialize
     {
         private readonly PeriodicTimer _periodicTimer;
+        private readonly List<ChatMessage> _messages;
 
         [ObservableProperty] private int _CurrentTime;
         [ObservableProperty] private bool _IsWritingResponse;
@@ -26,11 +26,16 @@ namespace dobra3.Sdk.ViewModels.Dialogs
             _question = question;
             _CurrentTime = 60;
             _periodicTimer = new(TimeSpan.FromMilliseconds(1000));
+            _messages = new()
+            {
+                new ChatMessage(ChatMessageRole.System, "Na pytanie `" + _question.Title + "` są cztery odpowiedzi: Odpowiedź A: " + _question.Answers[0].Text + ", B: " + _question.Answers[1].Text + ", C: " + _question.Answers[2].Text + ", D: " + _question.Answers[3].Text + ", Udawaj osobę która musi wybrać jedno z nich, nawet jak jesteś pewien odpowiedzi to używaj zwrotów `myślę, że`, `wydaje mi się że` itd. NIE wspominaj o byciu modelem SI, odpowiadaj krótko. Jeśli nie znasz odpowiedzi na pytanie to przyznaj się do swojej niewiedzy ale i tak wytypuj 1 z odpowiedzi jako najbardziej prawdopodobną. Jak znasz poprawną odpowiedź to uzasadnij w krótki sposób dlaczego ją wybrałeś.")
+            };
         }
 
         public Task InitAsync(CancellationToken cancellationToken = default)
         {
             _ = BeginTimerAsync(cancellationToken);
+            _ = SendAsync(null);
             return Task.CompletedTask;
         }
 
@@ -50,38 +55,27 @@ namespace dobra3.Sdk.ViewModels.Dialogs
         [RelayCommand]
         private async Task SendAsync(string? query)
         {
-            if (string.IsNullOrEmpty(query))
+            IsWritingResponse = true;
+            if (_messages.Count > 1 && query is null)
                 return;
 
-            IsWritingResponse = true;
-            var bubble = new ChatBubbleViewModel();
-            bubble.Message = await CallFriend(_question);
+            ChatBubbleViewModel bubble;
+            if (query is not null)
+            {
+                bubble = new ChatBubbleViewModel();
+                bubble.Message = query;
+                Bubbles.Add(bubble);
+                _messages.Add(new ChatMessage(ChatMessageRole.User, bubble.Message));
+            }
+
+            bubble = new ChatBubbleViewModel();
+            bubble.Message = await QuestionFriend();
             Bubbles.Add(bubble);
+            _messages.Add(new ChatMessage(ChatMessageRole.Assistant, bubble.Message));
             IsWritingResponse = false;
         }
-        
-        public async Task<string> CallFriend(QuestionViewModel qs)
-        {
-            var api = new OpenAI_API.OpenAIAPI(ApiKeys.GetOpenAiKey());
-            try { 
-                var result = await api.Chat.CreateChatCompletionAsync(new ChatRequest()
-                {
-                    Model = Model.ChatGPTTurbo,
-                    Temperature = 0.1,
-                    MaxTokens = 100,
-                    Messages = new ChatMessage[] {
-                        new ChatMessage(ChatMessageRole.User, "Na pytanie `" + qs.Title + "` są cztery odpowiedzi: Odpowiedź A: " + qs.Answers[0].Text + ", B: " + qs.Answers[1].Text + ", C: " + qs.Answers[2].Text + ", D: " + qs.Answers[3].Text + ", Udawaj osobę która musi wybrać jedno z nich, nawet jak jesteś pewien odpowiedzi to używaj zwrotów `myślę, że`, `wydaje mi się że` itd. NIE wspominaj o byciu modelem SI, odpowiadaj krótko. Jeśli nie znasz odpowiedzi na pytanie to przyznaj się do swojej niewiedzy ale i tak wytypuj 1 z odpowiedzi jako najbardziej prawdopodobną. Jak znasz poprawną odpowiedź to uzasadnij w krótki sposób dlaczego ją wybrałeś.")
-                    }
-                });
-                return result.ToString();
-            }
-            catch (Exception ex)
-            {
-                return ex.Message;
-            }
-        }
 
-        public async Task<string> QuestionFriend(string question)
+        private async Task<string> QuestionFriend()
         {
             var api = new OpenAI_API.OpenAIAPI(ApiKeys.GetOpenAiKey());
             try
@@ -91,9 +85,7 @@ namespace dobra3.Sdk.ViewModels.Dialogs
                     Model = Model.ChatGPTTurbo,
                     Temperature = 0.1,
                     MaxTokens = 100,
-                    Messages = new ChatMessage[] {
-                        new ChatMessage(ChatMessageRole.User, question)
-                    }
+                    Messages = _messages
                 });
                 return result.ToString();
             }
