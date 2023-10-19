0c1d39677568a054fe7b1930cae4ee8012f433f2
chlorcl <81020323+chlorcl@users.noreply.github.com>
2023-10-19 12:25:32 +0200

    Add main menu and settings dialog

---
+++ dobra3/Views/Dialogs/SettingsDialogWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace dobra3.Views.Dialogs
+{
+    public partial class SettingsDialogWindow : Window
+    {
+        public SettingsDialogWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
