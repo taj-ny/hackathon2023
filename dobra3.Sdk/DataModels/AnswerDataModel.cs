52fa8b642f27891e375f75b982e82e89d6049586
chlorcl <81020323+chlorcl@users.noreply.github.com>
2023-10-19 10:28:24 +0200

    Add DataModels

---
+++ dobra3.Sdk/DataModels/AnswerDataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace dobra3.Sdk.DataModels
+{
+    [Serializable]
+    public sealed record AnswerDataModel(string? Answer, bool? IsCorrect)
+    {
+        [JsonPropertyName("answer")]
+        public string? Answer { get; set; }
+
+        [JsonPropertyName("isCorrect")]
+        public bool? IsCorrect { get; set; }
+    }
+}
---
+++ dobra3.Sdk/DataModels/QuestionDataModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace dobra3.Sdk.DataModels
+{
+    [Serializable]
+    public sealed record QuestionDataModel(string? Question, int? Difficulty, List<AnswerDataModel>? Answers)
+    {
+        [JsonPropertyName("question")]
+        public string? Question { get; set; }
+
+        [JsonPropertyName("difficulty")]
+        public int? Difficulty { get; set; }
+
+
+        [JsonPropertyName("answers")]
+        public List<AnswerDataModel>? Answers { get; set; }
+    }
+}
